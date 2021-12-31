3a7e557101dc7556f33f26d36eda6dfe6944dc7d
thenerdone <poria9chegeni@gmail.com>
2021-12-31 17:49:10 +0330

    setup layout

--- pages/game/index.vue
+++ pages/game/index.vue
@@ -1,33 +1,43 @@
 <template>
   <Navbar>
-    <div>Enter number between {{ `${min} - ${max}` }}</div>
-    <div class="d-flex">
-      <CustomInput @input="guessedNum = $event" :value="guessedNum" />
-      <b-button variant="primary" block @click="handleNum">submit</b-button>
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-12 col-lg-6 col-md-8">
+          <div>Enter number between {{ `${min} - ${max}` }}</div>
+          <div class="d-flex">
+            <CustomInput @input="guessedNum = $event" :value="guessedNum" />
+            <b-button variant="primary" block @click="handleNum"
+              >submit</b-button
+            >
+          </div>
+          <div>Previous guesses: {{ spliter }}</div>
+          <div v-if="counter !== 0">
+            your number is {{ message }} random number!
+          </div>
+          <div v-if="counter !== 0">
+            resault:
+            {{
+              counter < 10
+                ? `guessed: ${counter}`
+                : counter == 10 && guessedNum == randomNum
+                ? "you win"
+                : "you lose!"
+            }}
+          </div>
+          <b-button v-if="counter == 10" variant="info" block @click="reset"
+            >reset</b-button
+          >
+        </div>
+      </div>
     </div>
-    <div>Previous guesses: {{ spliter }}</div>
-    <div v-if="counter !== 0">your number is {{ message }} random number!</div>
-    <div v-if="counter !== 0">
-      resault:
-      {{
-        counter < 10
-          ? `guessed: ${counter}`
-          : counter == 10 && guessedNum == randomNum
-          ? "you win"
-          : "you lose!"
-      }}
-    </div>
-    <b-button v-if="counter == 10" variant="info" block @click="reset"
-      >reset</b-button
-    >
   </Navbar>
 </template>
 
 <script>
 import CustomInput from "../../components/common/CustomInput.vue";
-import Navbar from '../../components/layout/Navbar.vue';
+import Navbar from "../../components/layout/Navbar.vue";
 export default {
-  components: { CustomInput,Navbar },
+  components: { CustomInput, Navbar },
   data() {
     return {
       counter: 0,
@@ -83,4 +93,8 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container.mt-5{
+  margin:5rem;
+}
+</style>
