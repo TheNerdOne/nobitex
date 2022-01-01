3d464f2df7f41ae9901e186d338e2851f50e900e
thenerdone <poria9chegeni@gmail.com>
2022-01-01 11:34:42 +0330

    fix prices functions and refresh every 30 sec

--- pages/prices/index.vue
+++ pages/prices/index.vue
@@ -1,28 +1,50 @@
 <template>
   <Navbar>
-    <Table :items="this.coinsData" />
+    <b-overlay :show="showTable" rounded="sm">
+      <Table :items="this.coinsData" />
+    </b-overlay>
   </Navbar>
 </template>
 
 <script>
 import Table from "@/components/common/Table.vue";
 import priceDataProvider from "@/services/prices";
-import Navbar from '../../components/layout/Navbar.vue';
+import Navbar from "../../components/layout/Navbar.vue";
 export default {
-  components: { Table,Navbar },
+  components: { Table, Navbar },
   data() {
     return {
       coinsData: [],
       currencies: ["btc", "eth", "bnb", "doge"],
       dstCurrency: 1, //1 => rls 0 => usdt
+      showTable: false,
+      killGetPrice: null,
     };
+  },
+  computed: {
+    currentDstCurrency() {
+      if (this.dstCurrency === 1) {
+        return "rls";
+      } else {
+        return "usdt";
+      }
+    },
   },
   methods: {
     getEachCoinData(payload) {
+      this.showTable = true;
       priceDataProvider
         .getCoinDetail(payload)
         .then((res) => {
           this.seprateKeyValue(res.data.stats);
+        })
+        .then(() => {
+            this.mostChangeInDay(this.coinsData);
+        })
+        .then(() => {
+          setTimeout(() => {
+            this.showTable = false;
+          }, 3000);
         })
         .catch((err) => {
           console.log(err);
@@ -31,17 +53,34 @@ export default {
     seprateKeyValue(payload) {
       this.coinsData = Object.values(payload);
       for (let i = 0; i < this.coinsData.length; i++) {
-        this.coinsData[i].symbol = Object.keys(payload)[i].slice(0,-4);
+        this.coinsData[i].symbol = Object.keys(payload)[i].slice(0, -4);
       }
+    },
+    mostChangeInDay(payload) {
+      let temp = [];
+      let maximum = 0;
+      temp = payload.map((item) => Number(item.dayChange));
+      maximum = Math.max(...temp);
+      payload.find((e) => Number(e.dayChange) === maximum).higherChange = true;
     },
   },
   beforeMount() {
-    let temp
-    temp = this.dstCurrency === 1 ? 'rls' : 'usdt'
+    this.showTable = true;
     this.getEachCoinData({
       srcCurrency: this.currencies.toString(),
-      dstCurrency: temp,
+      dstCurrency: "rls",
     });
+  },
+  mounted() {
+    this.killGetPrice = setInterval(() => {
+      this.getEachCoinData({
+        srcCurrency: this.currencies.toString(),
+        dstCurrency: this.currentDstCurrency,
+      });
+    }, 30000);
+  },
+  beforeDestroy() {
+    clearInterval(this.killGetPrice);
   },
 };
 </script>
