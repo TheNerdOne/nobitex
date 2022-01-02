ae0551ec13356c2265c27a340ee44935d9f9e774
thenerdone <poria9chegeni@gmail.com>
2022-01-02 21:15:09 +0330

    clean extra code

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,7 +6,6 @@ export const authComputed = {
         authModal: (state) => state.showAuthModal,
         profileData: (state) => state.personalInfo.profileData
     }),
-    // ...mapGetters('auth', ['SET_TOKEN','REMOVE_TOKEN']),
 }
 export const authMethods = mapActions('auth', [
     'setUserToken',
